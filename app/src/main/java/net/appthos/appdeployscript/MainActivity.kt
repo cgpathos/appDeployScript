4fd820121142989174976cfd7655163c096a87e9
pathos <neopathos@me.com>
2020-10-14 22:39:41 +0900

    versionName 코드에서 접근하기 추가

--- app/src/main/java/net/appthos/appdeployscript/MainActivity.kt
+++ app/src/main/java/net/appthos/appdeployscript/MainActivity.kt
@@ -2,10 +2,13 @@ package net.appthos.appdeployscript
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    txt_version.text = packageManager.getPackageInfo(packageName, 0).versionName
   }
 }
